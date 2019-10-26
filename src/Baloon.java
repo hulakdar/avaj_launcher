8bf155c20d333a3cf73fa3f052dc61aa01ac7b3e
hulakdar <skamoza@gmail.com>
2019-10-26 15:50:34 +0300

    wip

--- src/Baloon.java
+++ src/Baloon.java
@@ -10,19 +10,23 @@ public class Baloon extends Aircraft implements Flyable {
         String weather = weatherTower.getWeather(coordinates);
         if (weather.equals("SUN")) // ◦ SUN - Longitude increases with 2, Height increases with 4
         {
+            System.out.println(toString() + "Sunny.");
             coordinates.addLongitude(2);
             coordinates.addHeight(4);
         }
         else if (weather.equals("RAIN"))// ◦ RAIN - Height decreases with 5
         {
+            System.out.println(toString() + "Rainy, kinda.");
             coordinates.addLatitude(-5);
         }
         else if (weather.equals("FOG"))// ◦ FOG - Height decreases with 3
         {
+            System.out.println(toString() + "Foggy.");
             coordinates.addLatitude(-3);
         }
         else if (weather.equals("SNOW"))// ◦ SNOW - Height decreases with 15
         {
+            System.out.println(toString() + "Snowy.");
             coordinates.addHeight(-15);
         }
     }
